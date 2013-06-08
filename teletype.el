6ff657b7038e1a8d855c775a4c635cb182e82b2a
thesoftwarebin <andrear1979@hotmail.com>
2013-06-08 14:35:43 +0200

    Removed some redundant "progn" and "let". Thanks Sacha!

--- teletype.el
+++ teletype.el
@@ -19,62 +19,58 @@
 ;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defun teletype-char (c short-pause long-pause)
-  (let 
-      (
-       (med-pause 
-	(/ 
-	 (+ long-pause (* 2 short-pause)) 
-	 3.0)))
-    (progn 
-      (cond 
-       ((equal c (string-to-char ".")) (progn (insert (char-to-string c)) (sit-for long-pause)))
-       ((equal c (string-to-char ",")) (progn (insert (char-to-string c)) (sit-for med-pause)))
-       ((equal c (string-to-char "\n")) (progn (sit-for med-pause) (insert (char-to-string c)) (sit-for med-pause)))
-       (t (progn (insert (char-to-string c)) (sit-for short-pause)))))))
-
+  (let ((med-pause (/ (+ long-pause (* 2 short-pause)) 3.0)))
+    (cond
+     ((equal c ?.)
+      (insert (char-to-string c))
+      (sit-for long-pause))
+     ((equal c ?,)
+      (insert (char-to-string c))
+      (sit-for med-pause))
+     ((equal c ?\n)
+      (sit-for med-pause) (insert (char-to-string c))
+      (sit-for med-pause))
+     (t
+      (insert (char-to-string c))
+      (sit-for short-pause)))))
+ 
 (defun teletype-text (str short-pause long-pause)
-  (progn
-    (mapcar 
-     (lambda (strchar) 
-       (progn
-	 (teletype-char strchar short-pause long-pause)))
-     (string-to-list str))))
-
+  (mapcar
+   (lambda (strchar)
+     (teletype-char strchar short-pause long-pause))
+   (string-to-list str)))
+ 
 (defun teletype-demo (pause-between-keys pause-between-rows buffer-title text)
-  (let ()
-    (progn
-      (switch-to-buffer (create-file-buffer buffer-title))
-      (erase-buffer)
-      (teletype-text 
-       text
-       pause-between-keys 
-       pause-between-rows)))
-  nil)
-
+  "Print characters with a pause between each character."
+  (switch-to-buffer (get-buffer-create buffer-title))
+  (erase-buffer)
+  (teletype-text text pause-between-keys pause-between-rows))
+ 
 (defun teletype-run-demo ()
+  "Demonstrate teletyping."
   (interactive)
-  (teletype-demo 
-   0.1 
-   0.5 
-   "Emacs teletype demo" 
+  (teletype-demo
+   0.1
+   0.5
+   "Emacs teletype demo"
    (concat
     "Hello Emacs user,\n\n"
-    "  teletype-like printing can be useful to\n"
+    " teletype-like printing can be useful to\n"
     "keep the user focused on a brief tutorial.\n\n"
-    "  You are currently reading at 10 cps, with\n"
+    " You are currently reading at 10 cps, with\n"
     "pauses of 0.5 sec at punctuation and newlines.\n"
-    "  If this demo annoys you, stop it with C-g.\n\n"
-    "  To run your own demo, you may load teletype.el and\n"
+    " If this demo annoys you, stop it with C-g.\n\n"
+    " To run your own demo, you may load teletype.el and\n"
     "execute the following LISP function:\n\n"
-    "  (teletype-demo 0.1 0.5 demo-title all-your-text)\n\n"
+    " (teletype-demo 0.1 0.5 demo-title all-your-text)\n\n"
     "Future improvements could be:\n\n"
-    "  - read text from file\n"
-    "  - a resumable \"pause\" key\n"
-    "  - support for text coloring\n"
-    "  - support for typewriter sound (tick-tick-tick)\n"
-    "  - configurable regexps for pauses (so for example you\n"
-    "    don't have to ... wait so long at the ellipsys\n"
-    "    triple dots)\n\n"
-    "  Hope you enjoyed, regards.\n\n"
-    "        The Software Bin\n"
-    "  http://github.com/thesoftwarebin\n")))
+    " - read text from file\n"
+    " - a resumable \"pause\" key\n"
+    " - support for text coloring\n"
+    " - support for typewriter sound (tick-tick-tick)\n"
+    " - configurable regexps for pauses (so for example you\n"
+    " don't have to ... wait so long at the ellipsys\n"
+    " triple dots)\n\n"
+    " Hope you enjoyed, regards.\n\n"
+    " The Software Bin\n"
+    " http://github.com/thesoftwarebin\n")))
